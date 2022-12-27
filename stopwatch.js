02f5bc4da999e5c03791358a024ae23560c161f8
SurajSSingh <surajss2@illinois.edu>
2022-12-26 17:41:34 -0800

    Overhaul timer system
    Updated to include more settings, add helper functions to refactor certain parts, html updated

--- stopwatch.js
+++ stopwatch.js
@@ -7,14 +7,14 @@ const plugin = new NomiePlugin({
     uses: [
         "createNote", // Create a new Note in Nomie
         "onLaunch", // Run on each launch of Nomie
-        // "onNote",
-        // "searchNotes",
-        "getTrackable", // Get a specific tracker
-        // "getTrackableUsage", // Get the usage of a tracker
+        // "onNote", // Can listen for the note event - deactivate for now
+        // "searchNotes", // May be used to search through notes - deactivate for now
+        "getTrackable", // Get information about a specific tracker
+        // "getTrackableUsage", // May be used to get the usage of a tracker - deactivate for now
         "selectTrackables", // Select a tracker or some set of trackers
-        // "getLocation",
+        // "getLocation", // May be used if user allows location - deactivate for now
     ],
-    version: "0.4.4", // Mostly follows SemVer
+    version: "0.5.0", // Mostly follows SemVer
     addToCaptureMenu: true,
     addToMoreMenu: true,
     addToWidgets: true,
@@ -25,6 +25,8 @@ const SAVED_STOPWATCHES = "save_stopwatches";
 const SETTING_STOPWATCH_TRACKER_NAME = "stopwatch_tracker_name";
 const SETTING_ALLOW_NAMES = "allow_names";
 const SETTING_IMMEDIATELY_SAVE = "immediate_save";
+const SETTING_AUTOSTART = "auto_start_stopwatch";
+const SETTING_SHOW_ALERTS = "show_alerts";
 
 // Helper functions
 // Adapted from https://stackoverflow.com/questions/29816872/how-can-i-convert-milliseconds-to-hhmmss-format-using-javascript
@@ -45,7 +47,7 @@ function saveStopwatches(stopwatch_arr) {
 class Stopwatch {
     constructor(name = "", after_stopwatch = null) {
         this.name = name;
-        this.time_elapsed = 0;
+        this.time_elapsed = PetiteVue.reactive({ value: 0 });
         this.is_running = false;
         this.currently_saved_millisec = 0;
         this.after_stop = Array.isArray(after_stopwatch) ? after_stopwatch : [];
@@ -80,7 +82,7 @@ class Stopwatch {
     }
 
     get not_running_text() {
-        return this.time_elapsed == 0 ? "Start" : "Resume"
+        return this.time_elapsed.value == 0 ? "Start" : "Resume"
     }
 
     get clear_text() {
@@ -91,8 +93,8 @@ class Stopwatch {
         return "Save";
     }
 
-    reactive_formattedTime() {
-        let x = this.time_elapsed;
+    get reactive_formattedTime() {
+        let _ = this.time_elapsed.value;
         return this.formattedTime;
     }
 
@@ -108,96 +110,206 @@ class Stopwatch {
         this.last_unpause_time = new Date();
         this.is_running = true;
         this.timer = setInterval(() => {
-            this.time_elapsed += 500;
+            this.time_elapsed.value += 500;
         }, 500);
     }
-
+    toggle() {
+        this.is_running ? this.pause() : this.resume();
+    }
 }
 
 // Petite Vue Initialization
 PetiteVue.createApp({
+    // Fields
     current_stopwatches: [],
-    will_use_name: false,
-    save_note_immediately: false,
-    stopwatch_name: "#stopwatch",
     settings_open: false,
+    settings: {
+        will_use_name: {
+            storage_name: SETTING_ALLOW_NAMES,
+            value: false,
+            activateTitle: "True",
+            activateMessage: "True Msg",
+            deactivateTitle: "False",
+            deactivateMessage: "False Msg",
+            get title() {
+                return "Allow Naming Stopwatches";
+            },
+        },
+        save_note_immediately: {
+            storage_name: SETTING_IMMEDIATELY_SAVE,
+            value: false,
+            activateTitle: "True",
+            activateMessage: "True Msg",
+            deactivateTitle: "False",
+            deactivateMessage: "False Msg",
+            get title() {
+                return "Save Notes Immediately";
+            },
+        },
+        stopwatch_auto_start: {
+            storage_name: SETTING_AUTOSTART,
+            value: false,
+            activateTitle: "True",
+            activateMessage: "True Msg",
+            deactivateTitle: "False",
+            deactivateMessage: "False Msg",
+            get title() {
+                return "Auto Start Stopwatches";
+            },
+        },
+        show_alerts: {
+            storage_name: SETTING_SHOW_ALERTS,
+            value: true,
+            activateTitle: "True",
+            activateMessage: "True Msg",
+            deactivateTitle: "False",
+            deactivateMessage: "False Msg",
+            get title() {
+                return "Show General Alerts";
+            },
+        },
+    },
+    debug: false,
+    stopwatch_name: {
+        storage_name: SETTING_STOPWATCH_TRACKER_NAME,
+        value: "#stopwatch",
+    },
+
+    // Helper functions
+    tryRunAlert(title, message) {
+        if (this.show_alerts) plugin.alert(title, message);
+    },
+    debugLog(...message) {
+        if (this.debug) console.log(...message)
+    },
+    initSettings() {
+        this.settings.forEach(current_setting => {
+            // Try to get the saved item from storage if possible, or leave the current setting
+            this.settings[current_setting].value = plugin.storage.getItem(this.settings[current_setting].storage_name) ?? this.settings[current_setting].value;
+        });
+        this.debugLog(this.settings);
+    },
+    checkedAction(item, action) {
+        if (typeof item === "number") {
+            const index = item;
+            if (this.current_stopwatches[index]) {
+                const stopwatch = this.current_stopwatches[index];
+                action(stopwatch, index);
+            }
+            else {
+                this.tryRunAlert("Invalid stopwatch index", `Expected a number between 0 and ${this.current_stopwatches.length - 1} (inclusive); got this instead: ${index}!`);
+            }
+        }
+        else if (typeof item === "object") {
+            const stopwatch = item;
+            const index = this.current_stopwatches.indexOf(stopwatch);
+            if (index > -1) {
+                action(stopwatch, index);
+            }
+            else {
+                this.tryRunAlert("Invalid stopwatch", `Expected an active stopwatch!`);
+            }
+        }
+        else {
+            this.tryRunAlert("Invalid Item", `Item provided is not valid: ${item.toString()}`);
+        }
+    },
+    toggleSettingSave(setting) {
+        plugin.storage.setItem(setting.storage_name, setting.value);
+        this.tryRunAlert(
+            setting.value ? setting.activateTitle : setting.deactivateTitle,
+            setting.value ? setting.activateMessage : setting.deactivateMessage
+        );
+    },
+    stopwatchClassStyle(stopwatch) {
+        return stopwatch.running;
+    },
+
     // Initialization
     async mounted() {
         plugin.onRegistered(async () => {
             await plugin.storage.init();
-            this.current_stopwatches = JSON.parse(plugin.storage.getItem(SAVED_STOPWATCHES)) ?? [];
-            this.will_use_name = plugin.storage.getItem(SAVED_STOPWATCHES);
-            this.save_note_immediately = plugin.storage.getItem(SETTING_IMMEDIATELY_SAVE);
-            this.stopwatch_name = plugin.storage.getItem(SETTING_STOPWATCH_TRACKER_NAME);
+            // Assign all items if available or go with defaults
+            this.current_stopwatches = JSON.parse(plugin.storage.getItem(SAVED_STOPWATCHES)) || [];
+            this.stopwatch_name.value = plugin.storage.getItem(SETTING_STOPWATCH_TRACKER_NAME) ?? "#stopwatch";
+            this.initSettings();
+
+            this.tryRunAlert("Plugin Initialized", "Stopwatch plugin now registered and ready to use!");
         });
     },
 
     // Main page:
     // 1. Add a new stopwatch
-    async stopwatch_add_new(will_use_trackers = false) {
+    async stopwatch_add_new(using_stopwatch_template) {
+        // Try to get the name if naming is allowed
         let stopwatch_name = "";
-        if (this.will_use_name) {
-            let res = await plugin.prompt("Name Stopwatch", "What do you want to name your stopwatch?");
+        if (this.settings.will_use_name.value) {
+            let res = this.debug ? { value: "Debug Stopwatch Name Test" } : await plugin.prompt("Name Stopwatch", "What do you want to name your stopwatch?");
             if (res.value) {
                 stopwatch_name = res.value;
             }
         }
+        let stopwatch = null;
+        // If using a stopwatch template, get all trackers included with that timer
+        if (using_stopwatch_template) {
 
-        if (will_use_trackers) {
-            const trackers = await plugin.selectTrackables(null, true);
-            this.current_stopwatches.push(new Stopwatch(stopwatch_name, trackers));
+            const stopwatch_template = this.debug ? {} : await plugin.selectTrackable("tracker");
+            this.debugLog(stopwatch_template);
+            if (stopwatch_template) {
+                const trackers = [];
+                stopwatch = new Stopwatch(stopwatch_name, trackers);
+            }
         }
+        // Otherwise, ask for the trackers to include and push that up
         else {
-            this.current_stopwatches.push(new Stopwatch(stopwatch_name, []));
+            const trackers = this.debug ? [] : await plugin.selectTrackables(null, true);
+            stopwatch = new Stopwatch(stopwatch_name, trackers);
         }
-        plugin.alert(`New ${will_use_trackers ? "Specific" : "General"} Stopwatch created`, `A new stopwatch with name ${stopwatch_name} has been created!`);
+        stopwatch ??= new Stopwatch(stopwatch_name, []);
+        this.current_stopwatches.push(stopwatch);
+        // Auto start
+        if (this.settings.stopwatch_auto_start.value) stopwatch.resume();
+        // Show alert for new stopwatch and save the currently active ones 
+        this.tryRunAlert(`New ${using_stopwatch_template ? "Specific" : "Custom"} Stopwatch created`, `A new stopwatch with name ${stopwatch_name} has been created!`);
         saveStopwatches(this.current_stopwatches);
     },
     // 2. Clear a stopwatch
     async stopwatch_clear(index) {
-        if (this.current_stopwatches[index]) {
-            let will_delete = await plugin.confirm("Are you sure you want to delete this stopwatch?", "You will not be able recovery it.");
+        this.checkedAction(index, async (stopwatch, index) => {
+            // Ask to confirm deleting stopwatch
+            let will_delete = this.debug ? { value: true } : await plugin.confirm("Are you sure you want to delete this stopwatch?", "You will not be able recovery it.");
+            // If confirmed, delete the stopwatch at the specific index
             if (will_delete.value) {
                 this.current_stopwatches.splice(index, 1);
-                plugin.alert("Stopwatch Deleted!", "The stopwatch has been deleted.");
+                this.tryRunAlert("Stopwatch Deleted!", "The stopwatch has been deleted.");
             }
-        }
-        else {
-            plugin.alert("Invalid stopwatch index", `Expected a number between 0 and ${this.current_stopwatches.length - 1} (inclusive); got this instead: ${index}`);
-        }
+        });
+        // Save the currently running stopwatches
         saveStopwatches(this.current_stopwatches);
     },
     // 3. Save a finished stopwatch
     async stopwatch_save(index) {
-        if (this.current_stopwatches[index]) {
-            let stopwatch = this.current_stopwatches[index];
-            // console.log(stopwatch, stopwatch.after_stop);
+        this.checkedAction(index, async (stopwatch, index) => {
+            // If the
+            this.debugLog(stopwatch);
             let answers = [];
             for (const tracker of stopwatch.after_stop) {
-                // console.log(tracker);
-                answers.push(await plugin.getTrackableInput(tracker.id));
+                let answer = this.debug ? { note: "#DEBUG" } : await plugin.getTrackableInput(tracker.id);
+                this.debugLog(tracker, answer);
+                answers.push(answer);
             }
-            // console.log(answer_notes, answer_notes.toString());
             const name = stopwatch.name ? ` from ${stopwatch.name},` : "";
-            const answer_notes = answers.map(tracker => tracker.note).join(" ");
-            const resulting_note = `${this.stopwatch_name}(${stopwatch.formattedTime})${name}${answer_notes}`;
-            if (this.save_note_immediately) {
-                plugin.createNote({
-                    note: resulting_note,
-                    score: 0,
-                })
-            }
-            else {
-                plugin.openNoteEditor({
-                    note: resulting_note,
-                    score: 0,
-                })
-            }
+            const answer_notes = " " + answers.map(tracker => tracker.note).join(" ");
+            const final_note = {
+                note: `${this.stopwatch_name.value}(${stopwatch.formattedTime})${name}${answer_notes}`,
+                score: 0,
+            };
+            this.debugLog(answer_notes, final_note);
+            const note_function = this.settings.save_note_immediately.value ? plugin.createNote : plugin.openNoteEditor;
+            note_function(final_note);
             this.current_stopwatches.splice(index, 1);
-        }
-        else {
-            plugin.alert("Invalid stopwatch index", `Expected a number between 0 and ${this.current_stopwatches.length - 1} (inclusive); got this instead: ${index}`);
-        }
+        });
+        // Save the currently running stopwatches
         saveStopwatches(this.current_stopwatches);
     },
 
@@ -207,28 +319,12 @@ PetiteVue.createApp({
     },
     // 1. Change stopwatch tracker name
     async changeStopwatchTrackerName() {
-        let res = await plugin.prompt("Stopwatch Tracker Name Change", "What would you like the stopwatch tracker name to be called? (defaults to #stopwatch)");
+        let res = this.debug ? { res: this.stopwatch_name.value + "!" } : await plugin.prompt("Stopwatch Tracker Name Change", "What would you like the stopwatch tracker name to be called? (defaults to #stopwatch)");
         if (res.value) {
+            this.stopwatch_name.value = res.value || "#stopwatch";
             // Prepend "#" if not already there
-            const new_name = new_name.startsWith('#') ? res.value : "#" + res.value;
-            this.stopwatch_name = res.value || "#stopwatch";
-            plugin.storage.setItem(SETTING_STOPWATCH_TRACKER_NAME, this.stopwatch_name);
+            this.stopwatch_name.value = this.stopwatch_name.value.startsWith('#') ? this.stopwatch_name.value : "#" + this.stopwatch_name.value;
+            plugin.storage.setItem(SETTING_STOPWATCH_TRACKER_NAME, this.stopwatch_name.value);
         }
     },
-    // 2. Allow naming stopwatches
-    changeAllowNamingStopwatches(event) {
-        plugin.storage.setItem(SETTING_ALLOW_NAMES, this.will_use_name);
-        const title = this.will_use_name ? "Allowing Names" : "Disallowing Names";
-        const message = this.will_use_name ? "You can now name you stopwatches!" : "You cannot name your stopwatches.";
-        plugin.alert(title, message);
-    },
-    // 3. Allow saving notes immediately
-    changeSavingNotesImmediately(event) {
-        plugin.storage.setItem(SETTING_ALLOW_NAMES, this.save_note_immediately);
-        const title = this.save_note_immediately ? "Save Immediately" : "Show Notes";
-        const message = this.save_note_immediately ? "Your stopwatch notes will be saved immediately." : "Your stopwatch notes will show up and allow you to edit.";
-        plugin.alert(title, message);
-    },
-    // 4. Add stopwatch templates
-    // 5. Remove stopwatch template
 }).mount('#content')
